65bd89a4a0a585b0975ed744b733a534e661c76d
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-25 18:05:37 +0530

    added components , set up redux store

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import { BrowserRouter, Routes , Route} from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
+import Feed from "./Feed";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
    <>
+      <Provider store={appStore}>
 
-   <BrowserRouter basename="/">
-   <Routes>
-    <Route path="/" element={<Body/>} > 
-    <Route path="/login" element={<Login/>} />
-    <Route path="/profile" element={<Profile/>} />
-    </Route>
+        <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>} > 
+          <Route path="/" element={<Feed />} /> 
+          <Route path="/login" element={<Login/>} />
+          <Route path="/profile" element={<Profile/>} />
+          </Route>
 
-   </Routes>
-   </BrowserRouter>
-
+        </Routes>
+        </BrowserRouter>
+      </Provider>
    </>
     
   )
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,31 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "./utils/constants";
 
 const Login = () => {
 
-  const [emailId, setEmailId] = useState("");
-  const [password, setPassword] = useState("");
+  const [emailId, setEmailId] = useState("neelam@gmail.com");
+  const [password, setPassword] = useState("Neelam@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
+    
     try { 
-      const res = await axios.post("http://localhost:3000/login", {
+      const res = await axios.post(BASE_URL + "/login", 
+      {
         emailId,
         password,
-      } ,{
-        withCredentials:true,
-      });
+      } ,
+      { withCredentials:true }
+    );
+    console.log(res.data);
+    dispatch(addUser(res.data)); // to add data in the store
+    return navigate("/")
+
     } catch(err){
       console.error(err);
     }
