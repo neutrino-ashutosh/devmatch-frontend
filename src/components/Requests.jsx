c6ff9840c7ad34fb8bce568325af7486362492e8
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2025-08-27 19:05:36 +0530

    added functionality for accepting and rejecting connection requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,12 +1,26 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 import { useEffect } from "react";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
+
+
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(BASE_URL + "/request/review/" + status + "/" + _id,
+        {}, 
+        {withCredentials: true,}
+      );
+      dispatch(removeRequest(_id));
+    } catch(err){
+       console.log(err.message);
+     }
+  }
 
   const fetchRequests = async () => {
     try{
@@ -50,8 +64,8 @@ const Requests = () => {
               <p> {about} </p>
             </div>
              <div>
-              <button className="btn btn-primary mx-2"> Accept </button>
-              <button className="btn btn-secondary mx-2"> Reject </button>
+              <button className="btn btn-primary mx-2" onClick={() => reviewRequest("accepted", request._id)}> Accept </button>
+              <button className="btn btn-secondary mx-2" onClick={() => reviewRequest("rejected", request._id)}> Reject </button>
             </div>
           </div>
         )
